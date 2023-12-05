9bfd8538cdbd93c8e2411cfd443e5792c03158cf
Godsent Oko-Ose <111008769+Iamkinggee@users.noreply.github.com>
2023-12-05 07:59:00 +0100

    Update NavBar.js
    Home added

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,8 +3,7 @@ import { Link } from 'react-router-dom'
 import {BsTelephone,BsTelephoneFill,BsGearFill,BsGear} from 'react-icons/bs'
 import {GoFileDirectory,GoFileDirectoryFill} from 'react-icons/go'
 import{FaRegUser,FaUser} from 'react-icons/fa'
-
-
+import { GoHome } from "react-icons/go";
 
 
 // const YourComponent = () => {
@@ -58,11 +57,21 @@ const NavBar = () => {
   return (
     
 
-<div className='fixed z-50  bottom-3 rounded  w-full font-[poppins]  text-[10px]'>
+<div className='fixed z-50  bottom-0 rounded  w-full font-[poppins]  text-[10px]'>
 <div className='max-w-[1000px] flex justify-between items-center m-auto   border-gray-200 dark:bg-gray-700 dark:border-gray-600 opacity-90 '> 
 
 
-    <div class="grid h-full  grid-cols-4  font-medium p-2 px-2 m-auto ">
+    <div class="grid h-full  grid-cols-5  font-medium p-2 px-2 m-auto ">
+
+<Link to="/">  
+        <button type="button" class="inline-flex flex-col items-center justify-center  hover:bg-gray-50 dark:hover:bg-gray-700 group ">
+        
+         <GoHome className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-white" aria-hidden="true"/>
+            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-white  text-[10px]">Home</span>
+        </button>
+        </Link>
+
+    
   
     <Link to="/about">  
         <button type="button" class="inline-flex flex-col items-center justify-center  hover:bg-gray-50 dark:hover:bg-gray-700 group ">
