ad023f370351fd11d0bc802e86defa76134f724d
iamkinggee <iamgodsentt@gmail.com>
2023-12-05 08:40:26 +0100

    newchanges

--- src/Components/PagesFolder/HomePage.js
+++ src/Components/PagesFolder/HomePage.js
@@ -30,13 +30,13 @@ const HomePage = () => {
          <div className='w-full pb-[120px] '>
          <Slide right>
             <h1 className=' text-[14px] font-[LexendZetta] font-[100px] text-center md:text-start justify-center items-center text-white ' id='text-black'>Howdy, <br /> Godsent Oko-Ose here, i'm a</h1>
-            <h1 className='text-[40px] md:text-[60px]  font-bold opacity-15 leading-none text-center py-3 md:text-start text-white font-[LexendZetta] ' id='text-black'>Software <br />Developer</h1>
+            <h1 className='text-[40px] md:text-[60px]  font-bold opacity-15 leading-none text-center py-3 md:text-start text-white font-[LexendZetta] ' id='text-black'>Software <br />Developer.</h1>
 
             <div className='font-[LexendZetta] font-bold text-blue-500 text-center md:text-start'    > 
             <TypeWriter/>
              </div>
              </Slide>
-             
+
             <div className='mt-4 justify-center items-center m-auto text-center md:text-start'>
             <Slide right>
             <button className='border-2 border-blue-600 p-3 mt-3 font-semibold w-[300px] rounded bg-opacity-95 text-white cursor-pointer font-[LexendZetta] font-Light md:text-[14px] text-[11px] ' onClick={handlePageChange} id='text-black'>Lets Talk</button> 
